dabea827d379afae498211b5df6010c1018d2196
seanalvarez <alvie0818@gmail.com>
2021-01-12 11:34:28 -0500

    Enable custom tags, autoscaling

--- main.tf
+++ main.tf
@@ -31,6 +31,8 @@ data "terraform_remote_state" "db" {
 }
 
 data "template_file" "user_data" {
+count = var.enable_new_user_data ? 0 : 1
+
     template = file("${path.module}/user-data.sh")
 
     vars = {
@@ -40,11 +42,25 @@ data "template_file" "user_data" {
     }
 }
 
+data "template_file" "user_data_new" {
+    count = var.enable_new_user_data ? 1 : 0
+
+    template = file("${path.module}/user-data-new.sh")
+
+    vars = {
+        server_port = var.server_port
+    }
+}
+
 resource "aws_launch_configuration" "example" {
     image_id          = "ami-0c55b159cbfafe1f0"
     instance_type     = var.instance_type
     security_groups   = [aws_security_group.instance.id]
-    user_data         = data.template_file.user_data.rendered
+    user_data         =  (
+        length(data.template_file.user_data[*]) > 0
+            ? data.template_file.user_data[0].rendered
+            : data.template_file.user_data_new[0].rendered
+    )
 
     lifecycle {
         create_before_destroy = true
@@ -65,6 +81,16 @@ resource "aws_autoscaling_group" "example" {
         key                 = "Name"
         value               = "${var.cluster_name}-asg"
         propagate_at_launch = true
+    }
+
+    dynamic "tag" {
+        for_each = var.custom_tags
+
+        content {
+            key = tag.key
+            value = tag.value
+            propagate_at_launch = true
+        }
     }
 }
 
@@ -160,4 +186,62 @@ resource "aws_security_group_rule" "allow_http_inbound_instance" {
     to_port     = var.server_port
     protocol    = local.tcp_protocol
     cidr_blocks = local.all_ips
+}
+
+resource "aws_autoscaling_schedule" "scale_out_during_business_hours" {
+    count = var.enable_autoscaling ? 1 : 0
+
+    scheduled_action_name   = "${var.cluster_name}-scale-out-during-business-hours"
+    min_size                = 2
+    max_size = 10
+    desired_capacity = 10
+    recurrence = "0 9 * * *"
+    autoscaling_group_name = aws_autoscaling_group.example.name
+}
+
+resource "aws_autoscaling_schedule" "scale_in_at_night" {
+    count = var.enable_autoscaling ? 1 : 0
+
+    scheduled_action_name   = "${var.cluster_name}-scale-in-at-night"
+    min_size                = 2
+    max_size = 10
+    desired_capacity = 2
+    recurrence = "0 17 * * *"
+    autoscaling_group_name = aws_autoscaling_group.example.name
+}
+
+resource "aws_cloudwatch_metric_alarm" "high_cpu_utilization" {
+    alarm_name = "${var.cluster_name}-high-cpu-utilization"
+    namespace = "AWS/EC2"
+    metric_name = "CPUUtilization"
+
+    dimensions ={
+        AutoScalingGroupName = aws_autoscaling_group.example.name
+    }
+
+    comparison_operator = "GreaterThanThreshold"
+    evaluation_periods = 1
+    period             = 300
+    statistic = "Average"
+    threshold = 90
+    unit = "Percent"
+}
+
+resource "aws_cloudwatch_metric_alarm" "low_cpu_credit_balance" {
+    count = format("%.1s", var.instance_type) == "t" ? 1 : 0
+
+    alarm_name = "${var.cluster_name}-low-cpu-credit-balance"
+    namespace = "AWS/EC2"
+    metric_name = "CPUCreditBalance"
+
+    dimensions = {
+      "AutoScalingGroupName" = aws_autoscaling_group.example.name
+    }
+
+    comparison_operator = "LessThanThreshold"
+    evaluation_periods = 1
+    period = 300
+    statistic = "Minimum"
+    threshold = 10
+    unit = "Count"
 }
